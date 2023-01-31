18e6c15cdf03783a3e45a9ad3139e8a27b94e232
Peefy <xpf6677@163.com>
2023-01-31 16:34:51 +0800

    chore: update all libs exclude the windows platform.

--- install_lib_windows.go
+++ install_lib_windows.go
@@ -3,8 +3,6 @@ package kclvm_artifact
 func installLib(libDir, libName string) error {
 	libFullName := libName + ".dll"
 	exportLibFullName := libName + ".lib"
-	libFullPath := filepath.Join(libDir, libFullName)
-	exportLibFullPath := filepath.Join(libDir, libFullName)
 	err := writeLib(libDir, libFullName, kclvmCliLib)
 	if err != nil {
 		return err
