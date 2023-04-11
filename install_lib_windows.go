5a448e338f14f6b56fdbdce337acb66008397e7f
never <chenxiangfei.cxf@antgroup.com>
2023-04-11 19:48:44 +0800

    fix : fix installLib windows

--- install_lib_windows.go
+++ install_lib_windows.go
@@ -3,7 +3,7 @@ package kclvm_artifact
 func installLib(libDir, libName string, versionMatched bool) error {
 	libFullName := libName + ".dll"
 	exportLibFullName := libName + ".lib"
-	err := writeLib(libDir, libFullName, kclvmCliLib)
+	err := writeLib(libDir, libFullName, kclvmCliLib, versionMatched)
 	if err != nil {
 		return err
 	}
