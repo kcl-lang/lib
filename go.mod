8eab03759717e03ce6ae375a59ef8768348ae674
peefy <xpf6677@163.com>
2023-07-03 14:01:26 +0800

    chore: bump kcl-lang.io/kcl-artifact-go package path

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module kusionstack.io/kclvm-artifact-go
+module kcl-lang.io/kcl-artifact-go
 
-go 1.18
+go 1.19
--- lib/dummy.go
+++ lib/dummy.go
@@ -1,10 +1,10 @@
 package lib
 
 import (
-	_ "kusionstack.io/kclvm-artifact-go/lib/darwin-amd64"
-	_ "kusionstack.io/kclvm-artifact-go/lib/darwin-arm64"
-	_ "kusionstack.io/kclvm-artifact-go/lib/linux-amd64"
-	_ "kusionstack.io/kclvm-artifact-go/lib/linux-arm64"
-	_ "kusionstack.io/kclvm-artifact-go/lib/windows-amd64"
-	_ "kusionstack.io/kclvm-artifact-go/lib/windows-arm64"
+	_ "kcl-lang.io/kcl-artifact-go/lib/darwin-amd64"
+	_ "kcl-lang.io/kcl-artifact-go/lib/darwin-arm64"
+	_ "kcl-lang.io/kcl-artifact-go/lib/linux-amd64"
+	_ "kcl-lang.io/kcl-artifact-go/lib/linux-arm64"
+	_ "kcl-lang.io/kcl-artifact-go/lib/windows-amd64"
+	_ "kcl-lang.io/kcl-artifact-go/lib/windows-arm64"
 )
