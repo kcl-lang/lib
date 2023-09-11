a7615de0d4d55f09ba00b96e11ad26e8d5c4edbf
peefy <xpf6677@163.com>
2023-09-11 17:40:01 +0800

    chore: bump kcl libs to v0.6.0-alpha.1

--- install.go
+++ install.go
@@ -8,7 +8,7 @@ import (
 	"runtime"
 )
 
-const KCLVM_VERSION = "v0.5.6"
+const KCLVM_VERSION = "v0.6.0-alpha.1"
 
 func findPath(name string) string {
 	if path, err := exec.LookPath(name); err == nil {
