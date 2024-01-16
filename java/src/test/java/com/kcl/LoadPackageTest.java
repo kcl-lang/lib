22f58aa600cbfe2ad040e08516ab9d7919355873
peefy <xpf6677@163.com>
2024-01-16 22:33:27 +0800

    test: add ci tests for all languages
    Signed-off-by: peefy <xpf6677@163.com>

---
+++ java/src/test/java/com/kcl/LoadPackageTest.java
@@ -0,0 +1,24 @@
+package com.kcl;
+
+import org.junit.Test;
+
+import com.kcl.api.API;
+import com.kcl.api.Spec.LoadPackage_Args;
+import com.kcl.api.Spec.LoadPackage_Result;
+import com.kcl.api.Spec.ParseProgram_Args;
+import com.kcl.ast.Program;
+import com.kcl.util.JsonUtil;
+
+public class LoadPackageTest {
+    @Test
+    public void testProgramSymbols() throws Exception {
+		API api = new API();
+		LoadPackage_Result result = api.loadPackage(
+		    LoadPackage_Args.newBuilder().setResolveAst(true).setParseArgs(
+		    ParseProgram_Args.newBuilder().addPaths("./src/test_data/schema.k").build())
+		    .build());
+        Program program = JsonUtil.deserializeProgram(result.getProgram());
+        System.out.println(program.getRoot());
+		result.getSymbolsMap().values().forEach(s -> System.out.println(s));
+    }
+}
