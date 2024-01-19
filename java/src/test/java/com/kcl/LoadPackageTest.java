e1597ffdccfef810172fa98020bf552ab4db0d07
peefy <xpf6677@163.com>
2024-01-19 13:10:23 +0800

    feat: refactor symbol APIs
    Signed-off-by: peefy <xpf6677@163.com>

--- java/src/test/java/com/kcl/LoadPackageTest.java
+++ java/src/test/java/com/kcl/LoadPackageTest.java
@@ -1,11 +1,14 @@
 package com.kcl;
 
+import java.util.Collection;
+
 import org.junit.Test;
 
 import com.kcl.api.API;
 import com.kcl.api.Spec.LoadPackage_Args;
 import com.kcl.api.Spec.LoadPackage_Result;
 import com.kcl.api.Spec.ParseProgram_Args;
+import com.kcl.api.Spec.Symbol;
 import com.kcl.ast.Program;
 import com.kcl.util.JsonUtil;
 
@@ -17,9 +20,9 @@ public class LoadPackageTest {
                 LoadPackage_Args.newBuilder().setResolveAst(true).setParseArgs(
                         ParseProgram_Args.newBuilder().addPaths("./src/test_data/schema.k").build())
                         .build());
-        String programString = result.getProgram();
-        Program program = JsonUtil.deserializeProgram(programString);
+        Program program = JsonUtil.deserializeProgram(result.getProgram());
         System.out.println(program.getRoot());
-        result.getSymbolsMap().values().forEach(s -> System.out.println(s));
+        Collection<Symbol> symbols = result.getSymbolsMap().values();
+        symbols.forEach(s -> System.out.println(s));
     }
 }
