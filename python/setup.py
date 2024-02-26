1640fafba28b2252adfe33994b40033b210d51d5
peefy <xpf6677@163.com>
2024-02-26 11:21:14 +0800

    chore: update version to v0.8.0-alpha.2
    Signed-off-by: peefy <xpf6677@163.com>

--- python/setup.py
+++ python/setup.py
@@ -121,7 +121,7 @@ with open(require_path) as f:
 setup(
     name="kcl_lib",
     author="KCL Authors",
-    version="0.8.0-alpha.1",
+    version="0.8.0-alpha.2",
     license="Apache License 2.0",
     python_requires=">=3.7",
     description="KCL Artifact Library for Python",
