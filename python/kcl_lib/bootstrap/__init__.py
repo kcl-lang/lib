f8fc748d77d34423739d6523814fa06b44588979
peefy <xpf6677@163.com>
2024-02-26 15:17:33 +0800

    fix: kcl lib release folder
    Signed-off-by: peefy <xpf6677@163.com>

--- python/kcl_lib/bootstrap/__init__.py
+++ python/kcl_lib/bootstrap/__init__.py
@@ -3,9 +3,10 @@ import sys
 import platform
 from pathlib import Path
 
-KCLVM_VERSION = "0.8.0-alpha.4"  # You should replace this with actual version
+KCLVM_VERSION = "0.8.0-alpha.5"  # You should replace this with actual version
 KCLVM_CLI_BIN_PATH_ENV_VAR = "KCLVM_CLI_BIN_PATH"
 KCLVM_CLI_INSTALL_PATH_ENV_VAR = "KCLVM_CLI_INSTALL_PATH"
+KCLVM_CLI_USE_RELEASE_ENV_VAR = "KCLVM_CLI_USE_RELEASE"
 LIB_NAME = "kclvm_cli_cdylib"
 
 
--- python/kcl_lib/bootstrap/artifact.py
+++ python/kcl_lib/bootstrap/artifact.py
@@ -19,14 +19,18 @@ def is_amd64_arch():
 
 
 if sys.platform == "darwin":
-    if is_amd64_arch():
-        with open(f"{LIB_ROOT}/lib/darwin-amd64/libkclvm_cli_cdylib.dylib", "rb") as f:
-            DARWIN_AMD64_CLI_LIB = f.read()
-    else:
-        with open(f"{LIB_ROOT}/lib/darwin-arm64/libkclvm_cli_cdylib.dylib", "rb") as f:
-            DARWIN_ARM64_CLI_LIB = f.read()
 
     def cli_lib():
+        if is_amd64_arch():
+            with open(
+                f"{LIB_ROOT}/lib/darwin-amd64/libkclvm_cli_cdylib.dylib", "rb"
+            ) as f:
+                DARWIN_AMD64_CLI_LIB = f.read()
+        else:
+            with open(
+                f"{LIB_ROOT}/lib/darwin-arm64/libkclvm_cli_cdylib.dylib", "rb"
+            ) as f:
+                DARWIN_ARM64_CLI_LIB = f.read()
         return DARWIN_AMD64_CLI_LIB if is_amd64_arch() else DARWIN_ARM64_CLI_LIB
 
     def lib_path() -> str:
@@ -34,15 +38,18 @@ if sys.platform == "darwin":
             return str(LIB_ROOT.joinpath("lib").joinpath("darwin-amd64"))
         return str(LIB_ROOT.joinpath("lib").joinpath("darwin-arm64"))
 
+    def lib_name() -> str:
+        return "libkclvm_cli_cdylib.dylib"
+
 elif sys.platform.startswith("linux"):
-    if is_amd64_arch():
-        with open(f"{LIB_ROOT}/lib/linux-amd64/libkclvm_cli_cdylib.so", "rb") as f:
-            LINUX_AMD64_CLI_LIB = f.read()
-    else:
-        with open(f"{LIB_ROOT}/lib/linux-arm64/libkclvm_cli_cdylib.so", "rb") as f:
-            LINUX_ARM64_CLI_LIB = f.read()
 
     def cli_lib():
+        if is_amd64_arch():
+            with open(f"{LIB_ROOT}/lib/linux-amd64/libkclvm_cli_cdylib.so", "rb") as f:
+                LINUX_AMD64_CLI_LIB = f.read()
+        else:
+            with open(f"{LIB_ROOT}/lib/linux-arm64/libkclvm_cli_cdylib.so", "rb") as f:
+                LINUX_ARM64_CLI_LIB = f.read()
         return LINUX_AMD64_CLI_LIB if is_amd64_arch() else LINUX_ARM64_CLI_LIB
 
     def lib_path() -> str:
@@ -50,28 +57,39 @@ elif sys.platform.startswith("linux"):
             return str(LIB_ROOT.joinpath("lib").joinpath("linux-amd64"))
         return str(LIB_ROOT.joinpath("lib").joinpath("linux-arm64"))
 
+    def lib_name() -> str:
+        return "libkclvm_cli_cdylib.so"
+
 elif sys.platform == "win32":
-    if is_amd64_arch():
-        with open(f"{LIB_ROOT}/lib/windows-amd64/kclvm_cli_cdylib.dll", "rb") as f:
-            WINDOWS_AMD64_CLI_LIB = f.read()
-        with open(f"{LIB_ROOT}/lib/windows-amd64/kclvm_cli_cdylib.lib", "rb") as f:
-            WINDOWS_AMD64_EXPORT_LIB = f.read()
-    else:
-        with open(f"{LIB_ROOT}/lib/windows-arm64/kclvm_cli_cdylib.dll", "rb") as f:
-            WINDOWS_ARM64_CLI_LIB = f.read()
-        with open(f"{LIB_ROOT}/lib/windows-arm64/kclvm_cli_cdylib.lib", "rb") as f:
-            WINDOWS_ARM64_EXPORT_LIB = f.read()
 
     def cli_lib():
+        if is_amd64_arch():
+            with open(f"{LIB_ROOT}/lib/windows-amd64/kclvm_cli_cdylib.dll", "rb") as f:
+                WINDOWS_AMD64_CLI_LIB = f.read()
+        else:
+            with open(f"{LIB_ROOT}/lib/windows-arm64/kclvm_cli_cdylib.dll", "rb") as f:
+                WINDOWS_ARM64_CLI_LIB = f.read()
         return WINDOWS_AMD64_CLI_LIB if is_amd64_arch() else WINDOWS_ARM64_CLI_LIB
 
     def export_lib():
+        if is_amd64_arch():
+            with open(f"{LIB_ROOT}/lib/windows-amd64/kclvm_cli_cdylib.lib", "rb") as f:
+                WINDOWS_AMD64_EXPORT_LIB = f.read()
+        else:
+            with open(f"{LIB_ROOT}/lib/windows-arm64/kclvm_cli_cdylib.lib", "rb") as f:
+                WINDOWS_ARM64_EXPORT_LIB = f.read()
         return WINDOWS_AMD64_EXPORT_LIB if is_amd64_arch() else WINDOWS_ARM64_EXPORT_LIB
 
     def lib_path() -> str:
         if is_amd64_arch():
             return str(LIB_ROOT.joinpath("lib").joinpath("windows-amd64"))
         return str(LIB_ROOT.joinpath("lib").joinpath("windows-arm64"))
+
+    def lib_name() -> str:
+        return "kclvm_cli_cdylib.dll"
+
+    def export_lib_name() -> str:
+        return "kclvm_cli_cdylib.lib"
 
 else:
     raise f"Unsupported platform {sys.platform}, expected win32, linux or darwin platform"
