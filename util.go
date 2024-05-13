ffa7017f7e30a536979d223e5248ca57c9e175f5
Artem V. Navrotskiy <bozaro@yandex.ru>
2024-05-13 11:33:08 +0300

    Write library by temporary file creation to avoid in-place modification executed library
    Signed-off-by: Artem V. Navrotskiy <bozaro@yandex.ru>

--- util.go
+++ util.go
@@ -1,6 +1,7 @@
 package lib
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 )
@@ -8,22 +9,33 @@ import (
 func writeLib(libDir, libFullName string, content []byte, versionMatched bool) error {
 	libFullPath := filepath.Join(libDir, libFullName)
 	_, err := os.Stat(libFullPath)
-	if os.IsNotExist(err) || !versionMatched {
-		err = os.MkdirAll(libDir, 0777)
-		if err != nil {
-			return err
-		}
-		libFile, err := os.Create(libFullPath)
-		defer func() {
-			libFile.Close()
-		}()
-		if err != nil {
-			return err
-		}
-		_, err = libFile.Write(content)
-		if err != nil {
-			return err
-		}
+	if !os.IsNotExist(err) && versionMatched {
+		return err
 	}
-	return err
+	if err := os.MkdirAll(libDir, 0777); err != nil {
+		return err
+	}
+	for pass := 0; ; pass++ {
+		tmpFullPath := fmt.Sprintf("%s~%d", libFullPath, pass)
+		tmpFile, err := os.OpenFile(tmpFullPath, os.O_CREATE|os.O_RDWR|os.O_EXCL, 0755)
+		if err != nil {
+			if os.IsExist(err) {
+				continue
+			}
+			return err
+		}
+		defer func() {
+			tmpFile.Close()
+			_ = os.Remove(tmpFullPath)
+		}()
+
+		if _, err = tmpFile.Write(content); err != nil {
+			return err
+		}
+		if err := os.Rename(tmpFullPath, libFullPath); err != nil {
+			return err
+		}
+		break
+	}
+	return nil
 }
